62408d0b9d085e267f8c4c2147fceab622916b0e
Malab_Ubuntu <malab@outlook.com>
2023-01-24 22:10:30 +0530

    First operation implemented

--- main_6502.cpp
+++ main_6502.cpp
@@ -24,6 +24,14 @@ struct Mem
         // assert here Address is < MAX_MEM
         return Data[Address];
     }
+
+    // Write 1 byte
+    Byte& operator[]( u32 Address ) 
+    {
+        // assert here Address is < MAX_MEM
+        return Data[Address];
+    }
+    
 };
 
 struct CPU
@@ -46,26 +54,45 @@ struct CPU
 
     void Reset( Mem& memory )
     {
-        PC = 0XFFFC;
+        PC = 0xFFFC;
         SP = 0x0100;
         C = Z = I = D = B = V = N = 0;
         A = X = Y = 0;
         memory.Initialise();
     }
 
-    Byte FetchByte( u32 Cycles, Mem& memory )
+    Byte FetchByte( u32& Cycles, Mem& memory )
     {
         Byte Data = memory[PC];
         PC++;
-        Cycles++;
+        Cycles--;
         return Data;
     } 
+
+    // opcodes
+    static constexpr Byte INS_LDA_IM = 0xA9;
 
     void Execute( u32 Cycles, Mem& memory )
     {
         while ( Cycles > 0 )
         {
             Byte Instruction = FetchByte( Cycles, memory );
+
+            switch ( Instruction )
+            {
+            case INS_LDA_IM:
+            {
+                Byte Value = FetchByte(Cycles, memory);
+                A = Value;
+                Z = (A == 0);
+                N = (A & 0b1000000) > 0;
+            } break;
+            
+            default:
+            {
+                printf("Instruction not handled %d", Instruction);
+            } break;
+            }
         }
     }
 };
@@ -74,6 +101,8 @@ int main() {
     Mem mem;
     CPU cpu;
     cpu.Reset( mem );
+    mem[0xFFFC] = CPU::INS_LDA_IM;
+    mem[0xFFFD] = 0x42;
     cpu.Execute( 2, mem );
     return 0;
 }
